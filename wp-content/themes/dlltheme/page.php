b53f62770701a6563a737c57dde793bfcdca776e
Max Fowler <maxhfowler@gmail.com>
2020-05-19 15:41:44 +0200

    first commit

---
+++ wp-content/themes/dlltheme/page.php
@@ -0,0 +1,19 @@
+<?php
+get_template_part('head');
+?>
+
+<?php
+get_template_part('header');
+?>
+
+	<div class="page-wrapper info-page">
+        <?php
+            if ( have_posts() ) : while ( have_posts() ) : the_post();
+                get_template_part( 'page-content', get_post_format() );
+            endwhile; endif;
+        ?>
+	</div>
+
+<?php
+get_template_part('foot');
+?>
