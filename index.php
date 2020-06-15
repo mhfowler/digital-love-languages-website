23aaf652bd073ea54f37eb3ec4e74130c2455ba9
CommonMatter <hoffmelanie@gmail.com>
2020-06-15 01:28:10 -0400

    big css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ get_template_part('header');
 
 	<!-- wp:paragraph -->
 	<div class="description">
-<p>🦋&nbsp;&nbsp;Welcome to the online hub of Digital Love Languages, a 10 week online class at the <a href="https://sfpc.io/digitallove-spring2020/">School for Poetic Computation</a> taught by Melanie Hoff. <br />  <br />
+<p>🦋&nbsp;&nbsp;Welcome to the online hub of Digital Love Languages, a 10 week online class at the <a href="https://sfpc.io/digitallove-spring2020/">School for Poetic Computation</a> stewarded by Melanie Hoff, Max Fowler, Adina Glickstein, and Amber Officer-Narvasa.<br />  <br />
 	🦋&nbsp;&nbsp;As our daily activities and closest relationships become increasingly bound up with corporate systems of surveillance and exploitation, let’s explore and cultivate code as a love language that can be gentle, healing, and intimate. How do we want to live in a post COVID-19 world, and what role do we want technology to play? Digital Love Languages: Codes of Affirmation is a class about building poetic tools for online communion through a re-introduction to computers. This class is also a call to action for expanding computation’s capacity for fostering interdependence and feeling.</p>
 <!-- /wp:paragraph -->
 
