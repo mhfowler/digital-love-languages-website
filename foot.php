5d7e0456d62e35a3f4cba9a928440d1404ecab44
CommonMatter <hoffmelanie@gmail.com>
2020-06-28 02:55:27 -0400

    stats

--- foot.php
+++ foot.php
@@ -3,6 +3,17 @@
     <a class="broider header-link bottom-right" href="/library">Library</a>
 </div>
 
+
+<script type="text/javascript">
+var sc_project=12348812;
+var sc_invisible=1;
+var sc_security="c34d2a3d";
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+
+
 </body>
 
 </html>
