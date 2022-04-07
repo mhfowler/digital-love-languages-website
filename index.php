4383dfe11560aafbccdf93034bac696f80c6686e
CommonMatter <hoffmelanie@gmail.com>
2022-04-07 12:01:08 -0400

    pushing 2022 changes

--- index.php
+++ index.php
@@ -7,12 +7,73 @@ get_template_part('header');
 ?>
 
 <div class="center-pane">
+	<img id="poster-2022"src="<?php echo get_bloginfo('template_directory'); ?>/img/dll2022.png"</img>
+	<div class="description">
+<p>💧&nbsp;&nbsp;Welcome to 2022 the online hub of Digital Love Languages, a 10 week online class at the <a href="https://sfpc.io/digitallove-spring2020/">School for Poetic Computation</a> stewarded by Melanie Hoff, Max Fowler, & Megna Dholakia.<br />  <br />
+	💧&nbsp;&nbsp;Digital Love Languages: Communion, Consent, Refusal, Renewal is an introductory computing class for reimagining the core of technology’s purpose. This class is based on the premise that there is a world where all our software is made by people who love us and that we can contribute to building it. Digital Love Languages is an experiment in computational communion disguised as a class.</p>
+<!-- /wp:paragraph -->
 
-<div id="zoom-container">
-	<span class="heart">♡⚡️ </span><a target="_blank" class="zoom underline--dotted" href="https://digitallovelanguages.github.io/">Landscapes of Love Webzine</a><br />
-	<span class="heart">♡⚡️ </span><a target="_blank" class="zoom underline--dotted" href="https://rcravago.github.io/handle-with-care/inner-package.html">Love Letter to Digital Love Languages</a><br />
-	<span class="heart">♡⚡️ </span><a target="_blank" class="zoom underline--dotted" href="https://rcravago.github.io/handle-with-care/inner-package.html">Digital Love Languages on Are.na</a><br />
-	
+<!-- wp:paragraph -->
+<p>💧&nbsp;&nbsp;In this class, we will learn about things as a proxy to learn about other things. Learn about digital coding to learn how we’re socially coded. Learn about digital consent to learn about sexual consent. Learn about network protocols to learn about sharing. Learn about naming to learn about knowing. Learn about learning to learn about living.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>💧&nbsp;&nbsp;Together, we will build small, personal software with Python HTML Bash and Javascript for affirming one another across physical distance while making and sharing digital space with each other. We will engage with code as a craft capable of expressing a full range of feeling and desire. We will look to the love letter as form, from the&nbsp;<em class="special">quill</em>&nbsp;to the&nbsp;<em class="special">sext</em>. We will explore social and digital consent and ask: How are they interwoven?.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>💧&nbsp;&nbsp;Through a series of educational encounters, we will code poetic structures for digital touching and practice a re-examination of personal and networked computing. This is a call to action for expanding computation’s capacity for fostering interdependence and feeling among the immense personal and collective transformations brought on by COVID-19, collapse, and renewal.</p>
+<br /><br />
+<p>
+	<a class="big-link" href="https://lovelanguages.melaniehoff.com/syllabus-2/">Syllabus</a>
+</p>
+<br /><br />
+
+	</div>
+
+	<div class="image-history">
+<h2>Digital Love Languages History</h2>
+
+
+
+<img style="border: 1px dashed white" src="<?php echo get_bloginfo('template_directory'); ?>/img/2020.jpg"</img>
+<p style="font-weight: bold">
+	2020<br />Digital Love Languages: Codes of Affirmation<br />
+	With Melanie Hoff Max Fowler, Adina Glickstein, and Amber Officer-Narvasa
+	<br />10 Weeks online
+</p>
+
+<br />
+<br />
+<br />
+<br />
+
+
+<img style="border: 1px dashed white" src="<?php echo get_bloginfo('template_directory'); ?>/img/2021.png"</img>
+<p style="font-weight: bold">
+	2021<br />Digital Love Languages: Sensory Commons<br />
+	With Melanie Hoff and Olivia McKayla Ross
+	<br />5 Weeks in person NYC
+</p>
+
+
+	</div>
+
+
+	<img src="<?php echo get_bloginfo('template_directory'); ?>/img/sharing-crop.png"</img>
+	<p style="font-weight: bold">
+		<a href="https://www.are.na/block/7738518">Speculative Liberatory Learning Environment</a> by Hilary Wang
+	</p>
+
+	<br /><br /><br />	<br /><br /><br />	<br /><br /><br />	<br /><br /><br />	<br /><br /><br /><br /><br />	<br /><br /><br />	<br /><br /><br />
+		<br /><br /><br />
+<h2 style="margin-left: 1.5em;">Digital Love Languages 2020 Links</h2>
+
+<div style="margin-left: 2em;" id="zoom-container">
+	<span class="heart">♡ </span><a target="_blank" class="zoom underline--dotted" href="https://digitallovelanguages.github.io/">Landscapes of Love Webzine</a><br />
+	<span class="heart">♡ </span><a target="_blank" class="zoom underline--dotted" href="https://rcravago.github.io/handle-with-care/inner-package.html">Love Letter to Digital Love Languages</a><br />
+	<span class="heart">♡ </span><a target="_blank" class="zoom underline--dotted" href="https://2020lovelanguages.melaniehoff.com/">Digital Love Languages 2020 Website</a><br />
+
 </div>
 
 	<div>
@@ -30,34 +91,20 @@ get_template_part('header');
 	</li>
 	</ul>
 	</div>
-	
+
 	<!-- <div class="star-12"></div> -->
 </div>
 	</div>
 </div>
-	<img id="sharing-poster"src="<?php echo get_bloginfo('template_directory'); ?>/img/site.png"</img>
-	<img class="main-image" src=<?php echo get_bloginfo('template_directory'); ?>/img/dll.png></img>
+<br />
+<br /><br /><br /><br /><br /><br />
+	<br /><br /><br />	<br /><br /><br />	<br /><br /><br />	<br /><br /><br />	<br /><br />
+	<p style="font-size: 1.5em;" class="special">🩸&nbsp;&nbsp;What if all our software was made by people who love us?</p>
 
-	<!-- wp:paragraph -->
-	<div class="description">
-<p>🦋&nbsp;&nbsp;Welcome to the online hub of Digital Love Languages, a 10 week online class at the <a href="https://sfpc.io/digitallove-spring2020/">School for Poetic Computation</a> stewarded by Melanie Hoff, Max Fowler, Adina Glickstein, and Amber Officer-Narvasa.<br />  <br />
-	🦋&nbsp;&nbsp;As our daily activities and closest relationships become increasingly bound up with corporate systems of surveillance and exploitation, let’s explore and cultivate code as a love language that can be gentle, healing, and intimate. How do we want to live in a post COVID-19 world, and what role do we want technology to play? Digital Love Languages: Codes of Affirmation is a class about building poetic tools for online communion through a re-introduction to computers. This class is also a call to action for expanding computation’s capacity for fostering interdependence and feeling.</p>
-<!-- /wp:paragraph -->
+	<!-- <img id="sharing-poster"src="<?php echo get_bloginfo('template_directory'); ?>/img/site.png"</img>
+	<img class="main-image" src=<?php echo get_bloginfo('template_directory'); ?>/img/dll.png></img> -->
 
-<!-- wp:paragraph -->
-<p>🦋&nbsp;&nbsp;Together we will build small and personal software for affirming one another across physical distance. Technology will be unpacked as a social process, not only a logic or material, regarding code as a craft and medium capable of expressing the full range of feeling and desire.</p>
-<!-- /wp:paragraph -->
 
-<!-- wp:paragraph -->
-<p>🦋&nbsp;&nbsp;This course covers the building blocks of programming and natural language processing using Python & Javascript in the browser. Networks of people and computers become fertile sites for ambient and playful communication. We will look to the history of the love letter as a form, from the&nbsp;<em class="special">quill</em>&nbsp;to the&nbsp;<em class="special">sext</em>.<br />
-Through coding with langugage and in the browser, we can facilitate a kind of care that happens in small moments of routine.</p>
-<br /><br />
-<p>
-	<a class="big-link" href="/syllabus">Syllabus</a>
-</p>
-<br /><br />
-<p class="special">🩸&nbsp;&nbsp;What if all the software we used was made by people who love us?</p>
-	</div>
 <!-- /wp:paragraph -->
 </div>
 
